12315861ad7761464f316f03b8d5d4b1a77d4e31
Jocelyn FACCHINI <jocelyn.facchini@gmail.com>
2015-10-24 14:05:01 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,17 @@
+Battleship
+==========
+
+This project is for demonstration purpose.
+This is an implementation of the game Battleship for one player.
+
+Installation
+------------
+
+```bash
+composer install
+```
+
+Run the game
+------------
+
+Browse the file `web/index.php`
---
+++ src/Jfacchini/Battleship/Tests/BoardGameTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Jfacchini\Battleship\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Class BoardGameTest
+ * @package Jfacchini\Battleship\Tests
+ */
+class BoardGameTest extends PHPUnit_Framework_TestCase
+{
+    public function testAutoload()
+    {
+        $this->assertTrue(true);
+    }
+}
